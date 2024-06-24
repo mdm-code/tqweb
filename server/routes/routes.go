b731d3fd007453728044d4c9d716de45c40d07a3
mdm-code <code.madamczyk@gmail.com>
2024-06-24 23:09:38 +0200

    Added simple navbar

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -2,27 +2,18 @@ package routes
 
 import (
 	"github.com/labstack/echo/v4"
-	"github.com/mdm-code/tqweb/server/templates"
+	"github.com/mdm-code/tqweb/server/template"
 )
 
 // UseRootRoutes is a router grouping function.
 func UseRootRoutes(e *echo.Echo) {
 	e.GET("/", Index)
-	e.GET("/data", Data)
 	e.Static("/", "static")
 }
 
 // Index returns the landing page.
 func Index(c echo.Context) error {
-	component := templates.Index()
+	component := template.Page()
 	err := component.Render(c.Request().Context(), c.Response().Writer)
-	return err
-}
-
-// Data returns dummy data.
-func Data(c echo.Context) error {
-	p := templates.Person{Name: "Mike", Age: 22}
-	compoent := templates.Data(p)
-	err := compoent.Render(c.Request().Context(), c.Response().Writer)
 	return err
 }
