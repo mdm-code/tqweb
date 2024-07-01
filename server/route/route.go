25be9e25b80e94119d61322f2200ee5d25a87718
mdm-code <code.madamczyk@gmail.com>
2024-07-01 20:19:04 +0200

    Read TOML data from POST input

--- server/route/route.go
+++ server/route/route.go
@@ -1,7 +1,10 @@
 package route
 
 import (
+	"strings"
+
 	"github.com/labstack/echo/v4"
+	"github.com/mdm-code/tq/toml"
 	"github.com/mdm-code/tqweb/server/template"
 )
 
@@ -11,9 +14,32 @@ func UseRootRoutes(e *echo.Echo) {
 	e.Static("/", "static")
 }
 
+// UseInputRoutes groups together input processing handlers.
+func UseInputRoutes(e *echo.Echo) {
+	e.POST("/toml", CheckTOML)
+}
+
 // Index returns the landing page.
 func Index(c echo.Context) error {
 	component := template.Page()
 	err := component.Render(c.Request().Context(), c.Response().Writer)
 	return err
+}
+
+// CheckTOML verifies if the TOML data is syntactically correct.
+func CheckTOML(c echo.Context) error {
+	gt := toml.NewAdapter(
+		toml.NewGoTOML(
+			toml.GoTOMLConf{},
+		),
+	)
+	var tomlData any
+	value := c.FormValue("tomlData")
+	data := strings.NewReader(value)
+	err := gt.Unmarshal(data, &tomlData)
+	if err != nil {
+		return err
+	}
+	t := template.TomlInput(value)
+	return t.Render(c.Request().Context(), c.Response().Writer)
 }
--- server/server.go
+++ server/server.go
@@ -2,12 +2,15 @@ package server
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/mdm-code/tqweb/server/route"
 )
 
 // Server provides a dummy HTTP server.
 func Server() *echo.Echo {
 	e := echo.New()
+	e.Use(middleware.Logger())
 	route.UseRootRoutes(e)
+	route.UseInputRoutes(e)
 	return e
 }
