f0e8b9f9354c8e021362b84226908ec4c2fa935d
mdm-code <code.madamczyk@gmail.com>
2024-08-27 23:02:30 +0200

    Updated packages and moved to Go 1.23

--- vendor/github.com/a-h/templ/flake.nix
+++ vendor/github.com/a-h/templ/flake.nix
@@ -12,7 +12,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     xc = {
-      url = "github:joerdav/xc";
+      url = "github:joerdav/xc/v0.8.4";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -62,10 +62,10 @@
       devShell = forAllSystems ({ system, pkgs }:
         pkgs.mkShell {
           buildInputs = with pkgs; [
-            (golangci-lint.override { buildGoModule = buildGo121Module; })
+            golangci-lint
             cosign # Used to sign container images.
             esbuild # Used to package JS examples.
-            go_1_21
+            go_1_22
             gomod2nix.legacyPackages.${system}.gomod2nix
             gopls
             goreleaser
--- vendor/github.com/mdm-code/tq/toml/doc.go
+++
@@ -1,7 +0,0 @@
-/*
-Package toml declares an adapter for external TOML packages to accommodate for
-different implementations of data encoders and decoders. This limits the
-necessary code changes when swapping out implementations. This can possibly
-leave space for the user to configure the implementation they want to use.
-*/
-package toml
