52e084d09f333ffde8b56acf219d7e514f4f0f4c
mdm-code <code.madamczyk@gmail.com>
2024-08-27 23:02:58 +0200

    Kept basic echo and templ use on the server

--- server/route/route.go
+++ server/route/route.go
@@ -1,10 +1,19 @@
 package route
 
 import (
+	"net/http"
+
+	_ "github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 )
 
 // UseRootRoutes is a router grouping function.
 func UseRootRoutes(e *echo.Echo) {
 	e.Static("/", "static")
+	e.GET("/", Index)
+}
+
+// Index route for the application.
+func Index(c echo.Context) error {
+	return c.String(http.StatusOK, "OK")
 }
