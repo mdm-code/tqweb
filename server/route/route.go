420685c034af37602d660e9f428df48f8df52f5c
mdm-code <code.madamczyk@gmail.com>
2024-08-29 20:06:28 +0200

    Added routers for currently defined routes

--- server/route/route.go
+++ server/route/route.go
@@ -1,18 +1,54 @@
 package route
 
 import (
+	"bytes"
+	"net/http"
+	"strings"
+
 	"github.com/labstack/echo/v4"
+	"github.com/mdm-code/tq"
+	"github.com/mdm-code/tq/toml"
 	"github.com/mdm-code/tqweb/server/component"
 )
 
 const (
-	staticFsRoot = "assets"
+	staticFsRoot     = "assets"
+	staticPathPrefix = "/assets"
 )
 
-// UseRootRoutes is a router grouping function.
-func UseRootRoutes(e *echo.Echo) {
-	e.Static("/assets", staticFsRoot)
+// RegisterAll registers all routes defined for the HTTP server.
+func RegisterAll(e *echo.Echo) *echo.Echo {
+	return ServeStatics(
+		RegsiterRootRoutes(
+			RegisterProcessRoutes(
+				e,
+			),
+		),
+		staticFsRoot,
+		staticPathPrefix,
+	)
+}
+
+// ServeStatics registers a new route to serve static files from the root
+// directory at the provided path prefix.
+func ServeStatics(e *echo.Echo, at, from string) *echo.Echo {
+	e.Static(at, from)
+	return e
+}
+
+// RegsiterRootRoutes groups root routes.
+func RegsiterRootRoutes(e *echo.Echo) *echo.Echo {
 	e.GET("/", Index)
+	return e
+}
+
+// RegisterProcessRoutes groups data processing routes.
+func RegisterProcessRoutes(e *echo.Echo) *echo.Echo {
+	g := e.Group("/api/v1")
+	g.POST("/inputData", ProcessInputData)
+	g.POST("/tqQuery/validate", ValidateTqQuery)
+	g.POST("/toml/validate", ValidateTOML)
+	return e
 }
 
 // Index route for the tqweb.
